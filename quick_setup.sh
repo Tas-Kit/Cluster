17a17db7b55d12806be3614437a824d2563a1407
Lazy-Y <zhenyanz@usc.edu>
2018-04-19 20:27:59 -0700

    Update cluster

--- quick_setup.sh
+++ quick_setup.sh
@@ -1,6 +1,9 @@
 cd ..
 git clone https://github.com/Tas-Kit/webfront.git
 git clone https://github.com/Tas-Kit/proxyserver.git
+git clone https://github.com/Tas-Kit/authserver.git
+git clone https://github.com/Tas-Kit/webmain.git
+git clone https://github.com/Tas-Kit/taskservice.git
 
 cd cluster
 docker-compose up
