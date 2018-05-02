b2e15ef765c98813ed30f84cd9e9cf947f85d38e
Lazy-Y <zhenyanz@usc.edu>
2018-05-01 22:13:37 -0700

    Add nginx

--- quick_setup.sh
+++ quick_setup.sh
@@ -4,6 +4,7 @@ git clone https://github.com/Tas-Kit/proxyserver.git
 git clone https://github.com/Tas-Kit/authserver.git
 git clone https://github.com/Tas-Kit/webmain.git
 git clone https://github.com/Tas-Kit/taskservice.git
+git clone https://github.com/Tas-Kit/nginx-proxy.git
 
 cd cluster
 docker-compose up
